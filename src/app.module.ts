2ee4d19487f9f10a309468967c49351470af0abc
arfaz26 <chougulearfaz@gmail.com>
2022-07-18 22:40:03 +0530

    add middleware to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,10 +7,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
@@ -31,4 +43,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ReportsModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['wgwrhwrieib'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
